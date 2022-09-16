977a36d08ccd0baa8764c0f54d9fb91306d3f480
Nelo Puchades <correo@nelo.dev>
2022-09-16 20:28:55 +0200

    Creating and booting PHP with basic echo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello, World";
+        ?>
+    </h1>
+</body>
+</html>
